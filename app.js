01bb28051451bbced7361285ee878122299c8c0e
owais24 <sdowais36@gmail.com>
2020-01-28 11:27:17 +0530

    My first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+function clock(){
+const fullDate = new Date();
+ var hours= fullDate.getHours();
+var mins = fullDate.getMinutes();
+ var secs = fullDate.getSeconds();
+
+if(hours <10) {
+
+    hours= "0" +hours;
+    
+}
+if(mins <10){
+
+    mins = "0" + mins;
+}
+
+if(secs < 10){
+
+    secs = "0" + secs;
+}
+
+
+document.getElementById('hours').innerHTML= hours;
+document.getElementById('minutes').innerHTML= ":" + mins;
+document.getElementById('seconds').innerHTML= ":" + secs;
+
+
+}
+
+setInterval(clock,100);
